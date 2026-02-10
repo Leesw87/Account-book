bb164c1bce1f9a23115bd992adec32a717e85b61
Leesw87 <99631893+Leesw87@users.noreply.github.com>
2026-02-10 11:15:16 +0900

    Rename supabase variable to supabaseClient

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://kbjtxatyeemwrcbdokwy.supabase.co";
 const SUPABASE_KEY = "sb_publishable_bKhAbzuLGoHqXaoSmgEp0Q_6ed9VnUB";
 
-const supabase = supabase.createClient(
+const supabaseClient = supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
