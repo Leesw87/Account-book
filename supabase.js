3a4540acf90429c0504892fe169c3754cbf4db28
Leesw87 <99631893+Leesw87@users.noreply.github.com>
2026-02-10 11:17:08 +0900

    Expose supabaseClient to the global window object

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://kbjtxatyeemwrcbdokwy.supabase.co";
 const SUPABASE_KEY = "sb_publishable_bKhAbzuLGoHqXaoSmgEp0Q_6ed9VnUB";
 
-const supabaseClient = supabase.createClient(
+window.supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
